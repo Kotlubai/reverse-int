e15efc43188f43b4cd98b098a16fc3abef8f7c67
Kotlubai <119881185+Kotlubai@users.noreply.github.com>
2023-02-09 02:10:29 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    if (String(n).charAt(0) == "-") { 
+    return String(n).split("").reverse().join("").slice(0, -1);;
+    }
+    return String(n).split("").reverse().join("");  
 }
